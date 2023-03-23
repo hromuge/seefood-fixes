0cc6b1d076cf3fffad00f27fe4e414f87e7bf97a
Yassin Butt <butt@glue.ch>
2023-03-23 17:02:31 +0100

    add file size limit of 1 MB (file dimension limit of 1024px x 1024px wip)

--- apps/hotdog/src/app/upload/upload.component.ts
+++ apps/hotdog/src/app/upload/upload.component.ts
@@ -17,20 +17,29 @@ export class UploadComponent {
   reader = new FileReader();
   url?: any;
   loading = false;
+  maxFileSize = 1048576;
 
   setFile(fileInputElement: any) {
     this.res = undefined;
     if (fileInputElement && fileInputElement !== '') {
-      this.file = fileInputElement.files[0]
-      this.reader.readAsDataURL(fileInputElement.files[0]);
-      this.reader.onload = () => {
-        this.url = this.reader.result;
+      if (fileInputElement.files[0].size > this.maxFileSize) {
+        alert("File size is too big! Please upload a file that is smaller than 1MB.")
+        throw Error("file size > 1MB")
+      } else {
+        this.file = fileInputElement.files[0]
+        this.reader.readAsDataURL(fileInputElement.files[0]);
+        this.reader.onload = () => {
+          this.url = this.reader.result;
+        }
       }
     }
   }
 
   async classify() {
-    if (!this.file) throw Error("file is undefined");
+    if (!this.file) {
+      alert("Please upload a file.")
+      throw Error("file is undefined");
+    }
     this.loading = true;
     this.res = await this.classifier.classifyHotdog(this.file)
     this.loading = false;
