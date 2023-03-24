28de2d4884cd5cd46fa73e3ba4796aa721cf1cdc
Yassin Butt <butt@glue.ch>
2023-03-24 12:46:44 +0100

    add image size limit of 1024x1024px

--- apps/hotdog/src/app/upload/upload.component.ts
+++ apps/hotdog/src/app/upload/upload.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {ClassifierService} from "../classifier.service";
 import {HotdogClassification} from "@seefood/api-interfaces";
 
@@ -12,24 +12,36 @@ export class UploadComponent {
   constructor(private classifier: ClassifierService) {
   }
 
+  @ViewChild('uploadedImage')
+  imageElement?: ElementRef;
   file?: File
   res?: HotdogClassification
-  reader = new FileReader();
-  url?: any;
+  img?: any;
   loading = false;
   maxFileSize = 1048576;
+  maxImageResolution = 1024;
 
   setFile(fileInputElement: any) {
     this.res = undefined;
     if (fileInputElement && fileInputElement !== '') {
       if (fileInputElement.files[0].size > this.maxFileSize) {
+        this.img = null;
         alert("File size is too big! Please upload a file that is smaller than 1MB.")
         throw Error("file size > 1MB")
       } else {
+        const reader = new FileReader();
         this.file = fileInputElement.files[0]
-        this.reader.readAsDataURL(fileInputElement.files[0]);
-        this.reader.onload = () => {
-          this.url = this.reader.result;
+        reader.readAsDataURL(fileInputElement.files[0]);
+        reader.onload = () => {
+          this.img = new Image();
+          this.img.src = reader.result;
+          this.img.onload = () => {
+            if (this.img.width > this.maxImageResolution || this.img.height > this.maxImageResolution) {
+              this.img = null;
+              alert("Image is too large! Please only upload images with a resolution of 1024x1024 or smaller.");
+              throw Error("image resolution > 1024x1024px");
+            }
+          };
         }
       }
     }
